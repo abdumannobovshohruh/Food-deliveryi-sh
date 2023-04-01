6e11ea6ce5dd4a14cf63189b380b7366de98134d
shohruh973 <abdumannobovshohruh973@gmail.com>
2023-04-01 16:02:50 +0500

    wow

---
+++ script.js
@@ -0,0 +1,25 @@
+// let sabzi = document.querySelector(".sabzi")
+// let qulupnay = document.querySelector(".qulupnay")
+// sabzi.onclick = () => {
+//     sabzi.style.display = "none"
+//     qulupnay.style.display = "block"
+// }
+// qulupnay.onclick = () => {
+//     sabzi.style.display = "block"
+//     qulupnay.style.display = "none"
+// }
+// let logo = document.querySelector(".logo")
+// let menuright = document.querySelector(".menuRight")
+// console.log(logo);
+// let sabzi = document.querySelector(".sabzi")
+// let qulupnay = document.querySelector(".qulupnay")
+// sabzi.onclick = () => {
+//     sabzi.style.display = "none"
+//     menuright.style.display = "block"
+//     qulupnay.style.display = "block"
+// }
+// qulupnay.onclick = () => {
+//     sabzi.style.display = "block"
+//     menuright.style.display = "none"
+//     qulupnay.style.display = "none"
+// }
